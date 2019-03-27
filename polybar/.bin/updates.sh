3637ee0e12df23bfd47df3ba4e89fb1ccf003850
Łukasz Klimek <klinn91@gmail.com>
2019-03-28 00:15:03 +0100

    added desktop setup

---
+++ polybar/.bin/updates.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+BAR_ICON=""
+
+checkupdates() { apt list --upgradable | tail -n +2; }
+get_total_updates() { UPDATES=$(checkupdates 2>/dev/null | wc -l); }
+
+while true; do
+    get_total_updates
+
+    # when there are updates available
+    # every 10 seconds another check for updates is done
+    while (( UPDATES > 0 )); do
+        echo "$BAR_ICON $UPDATES"
+        sleep 10
+        get_total_updates
+    done
+
+    # when no updates are available, use a longer loop, this saves on CPU
+    # and network uptime, only checking once every 30 min for new updates
+    while (( UPDATES == 0 )); do
+        echo $BAR_ICON
+        sleep 1800
+        get_total_updates
+    done
+done
