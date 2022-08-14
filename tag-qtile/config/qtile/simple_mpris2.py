33188d1472516944d6d9b0aa7c84b94ac19d5956
Łukasz Klimek <klinn91@gmail.com>
2022-08-14 16:43:13 +0200

    fixed mpris (spotify) widget

--- tag-qtile/config/qtile/simple_mpris2.py
+++ tag-qtile/config/qtile/simple_mpris2.py
@@ -1,6 +1,10 @@
+from asyncio import get_event_loop, new_event_loop
+from typing import Callable
+
 from dbus_next.aio.message_bus import MessageBus
 from dbus_next.constants import BusType, MessageType
 from dbus_next.message import Message
+
 from libqtile.widget import base
 
 def _default_formatter(status, artist, song):
@@ -15,14 +19,7 @@ class PlaybackStatus:
     Playing = 'Playing'
     Stopped = 'Stopped'
 
-class SimpleMpris2(base._TextBox):
-    orientations = base.ORIENTATION_HORIZONTAL
-    defaults = [
-        ('name', 'spotify', 'Name of the widget.'),
-        ('objname', 'org.mpris.MediaPlayer2.spotify', 'DBUS MSPRIS 2 compatible player identifier.'),
-        ('display_formatter', _default_formatter, 'Function formatting the song data.'),
-    ]
-
+class MprisTracker:
     player_path = '/org/mpris/MediaPlayer2'
     player_interface = 'org.mpris.MediaPlayer2.Player'
     properties_interface = 'org.freedesktop.DBus.Properties'
@@ -31,13 +28,14 @@ class SimpleMpris2(base._TextBox):
     tracking_interface = 'org.freedesktop.DBus.Monitoring'
     signal_tracking_interface = 'org.freedesktop.DBus'
 
-    def __init__(self, **config):
-        base._TextBox.__init__(self, '', **config)
-        self.add_defaults(SimpleMpris2.defaults)
-        self.song_data = None
+    def __init__(self, objname: str, song_handler: Callable[[str, str, str], None]):
+        self.objname = objname
+        self.song_handler = song_handler
+        self.last_status = None
+        self.last_artist = None
+        self.last_song = None
 
     async def _config_async(self):
-        self._update_text(None, None, None)
         await self._track_song_changes()
         await self._fetch_current_song()
         await self._track_name_acquisition()
@@ -45,28 +43,28 @@ class SimpleMpris2(base._TextBox):
     async def _fetch_current_song(self):
         try:
             bus = await MessageBus(bus_type=BusType.SESSION).connect()
-            introspection = await bus.introspect(self.objname, SimpleMpris2.player_path)
-            obj = bus.get_proxy_object(self.objname, SimpleMpris2.player_path, introspection)
-            props_iface = obj.get_interface(SimpleMpris2.properties_interface)
-            props = await props_iface.call_get_all(SimpleMpris2.player_interface)
-            self._handle_prop_change(SimpleMpris2.player_interface, props, [])
+            introspection = await bus.introspect(self.objname, MprisTracker.player_path)
+            obj = bus.get_proxy_object(self.objname, MprisTracker.player_path, introspection)
+            props_iface = obj.get_interface(MprisTracker.properties_interface)
+            props = await props_iface.call_get_all(MprisTracker.player_interface)
+            self._handle_prop_change(MprisTracker.player_interface, props, [])
             bus.disconnect()
         except:
-            self._update_text(None, None, None)
+            self.song_handler(None, None, None)
 
     async def _track_song_changes(self):
         bus = await MessageBus(bus_type=BusType.SESSION).connect()
-        introspection = await bus.introspect(SimpleMpris2.tracking_name, SimpleMpris2.tracking_path)
-        obj = bus.get_proxy_object(SimpleMpris2.tracking_name, SimpleMpris2.tracking_path, introspection)
-        iface = obj.get_interface(SimpleMpris2.signal_tracking_interface)
+        introspection = await bus.introspect(MprisTracker.tracking_name, MprisTracker.tracking_path)
+        obj = bus.get_proxy_object(MprisTracker.tracking_name, MprisTracker.tracking_path, introspection)
+        iface = obj.get_interface(MprisTracker.signal_tracking_interface)
         await iface.call_add_match(f"type='signal',member='PropertiesChanged',sender='{self.objname}'")
         bus.add_message_handler(self._handle_prop_change_message)
 
     async def _track_name_acquisition(self):
         bus = await MessageBus(bus_type=BusType.SESSION).connect()
-        introspection = await bus.introspect(SimpleMpris2.tracking_name, SimpleMpris2.tracking_path)
-        obj = bus.get_proxy_object(SimpleMpris2.tracking_name, SimpleMpris2.tracking_path, introspection)
-        iface = obj.get_interface(SimpleMpris2.tracking_interface)
+        introspection = await bus.introspect(MprisTracker.tracking_name, MprisTracker.tracking_path)
+        obj = bus.get_proxy_object(MprisTracker.tracking_name, MprisTracker.tracking_path, introspection)
+        iface = obj.get_interface(MprisTracker.tracking_interface)
         await iface.call_become_monitor([f"type='signal',member='NameLost',arg0='{self.objname}'"], 0)
         bus.add_message_handler(self._handle_name_change_message)
 
@@ -75,16 +73,45 @@ class SimpleMpris2(base._TextBox):
             self._handle_prop_change(*msg.body)
 
     def _handle_prop_change(self, iface, changed_properties: dict, _):
-        if iface == SimpleMpris2.player_interface:
-            status = changed_properties.get('PlaybackStatus').value
-            meta = changed_properties.get('Metadata').value
-            artist = meta.get('xesam:artist').value[0]
-            song = meta.get('xesam:title').value
-            self._update_text(status, artist, song)
+        if iface == MprisTracker.player_interface:
+            print('---', repr(changed_properties))
+
+            status = self.last_status
+            if 'PlaybackStatus' in changed_properties:
+                status = self.last_status = changed_properties.get('PlaybackStatus').value or None
+
+            artist = self.last_artist
+            song = self.last_song
+            if 'Metadata' in changed_properties:
+                meta = changed_properties.get('Metadata').value
+                artist = self.last_artist = meta.get('xesam:artist').value[0] or None
+                song = self.last_song = meta.get('xesam:title').value or None
+
+            self.song_handler(status, artist, song)
+            if status is None or artist is None or song is None:
+                get_event_loop().create_task(self._fetch_current_song())
 
     def _handle_name_change_message(self, msg: Message):
         if msg.message_type == MessageType.SIGNAL and msg.member == 'NameLost':
-            self._update_text(None, None, None)
+            self.song_handler(None, None, None)
+
+class SimpleMpris2(base._TextBox):
+    orientations = base.ORIENTATION_HORIZONTAL
+    defaults = [
+        ('name', 'spotify', 'Name of the widget.'),
+        ('objname', 'org.mpris.MediaPlayer2.spotify', 'DBUS MSPRIS 2 compatible player identifier.'),
+        ('display_formatter', _default_formatter, 'Function formatting the song data.'),
+    ]
+
+    def __init__(self, **config):
+        base._TextBox.__init__(self, '', **config)
+        self.add_defaults(SimpleMpris2.defaults)
+        self.song_data = None
+        self.tracker = MprisTracker(self.objname, self._update_text)
+
+    async def _config_async(self):
+        self._update_text(None, None, None)
+        await self.tracker._config_async()
 
     def _update_text(self, status, artist, song):
         new = status, artist, song
@@ -99,3 +126,17 @@ class SimpleMpris2(base._TextBox):
             artist=self.song_data[1],
             song=self.song_data[2],
         )
+
+if __name__ == '__main__':
+    def formatter(status, artist, song):
+        print(_default_formatter(status, artist, song) or '-')
+
+    loop = new_event_loop()
+    tracker = MprisTracker('org.mpris.MediaPlayer2.spotify', formatter)
+
+    try:
+        loop.run_until_complete(tracker._config_async())
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
