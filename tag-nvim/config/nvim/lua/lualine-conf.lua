7077d6caa3070865aecda78c56b7ebe2ecd19f08
Łukasz Klimek <klinn91@gmail.com>
2023-01-08 14:04:23 +0100

    switched lualine to global instead of per-window

--- tag-nvim/config/nvim/lua/lualine-conf.lua
+++ tag-nvim/config/nvim/lua/lualine-conf.lua
@@ -18,7 +18,7 @@ require('lualine').setup {
         },
         ignore_focus = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
         refresh = {
             statusline = 1000,
             tabline = 1000,
@@ -44,5 +44,8 @@ require('lualine').setup {
     tabline = {},
     winbar = {},
     inactive_winbar = {},
-    extensions = {}
+    extensions = {
+        'nvim-dap-ui',
+        'nvim-tree',
+    },
 }
