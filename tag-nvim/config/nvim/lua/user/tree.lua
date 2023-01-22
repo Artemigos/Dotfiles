277904bd8a943307c13c739be45c8e34b524dc37
Łukasz Klimek <klinn91@gmail.com>
2023-01-22 17:13:19 +0100

    tweaked nvim-tree settings

--- tag-nvim/config/nvim/lua/user/tree.lua
+++ tag-nvim/config/nvim/lua/user/tree.lua
@@ -1,10 +1,16 @@
 local u = require('user.utils')
 
 require('nvim-tree').setup {
+    sync_root_with_cwd = true,
     view = {
         side = 'right',
         width = 45,
-    }
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+        show_on_open_dirs = false,
+    },
 }
 
 u.nmap('<Leader>e', ':NvimTreeToggle<CR>')
