156e465d12e14f7073088dbe3e644463f2b71ca0
Łukasz Klimek <klinn91@gmail.com>
2023-03-03 22:27:35 +0100

    lazy-fied general plugins

--- tag-nvim/config/nvim/lua/plugins/general.lua
+++ tag-nvim/config/nvim/lua/plugins/general.lua
@@ -1,11 +1,11 @@
 return {
-    { 'tpope/vim-repeat' },
-    { 'ggandor/lightspeed.nvim' },
-    { 'tpope/vim-surround' },
-    { 'wellle/targets.vim' },
+    { 'tpope/vim-repeat', event = 'VeryLazy' },
+    { 'ggandor/lightspeed.nvim', event = 'VeryLazy' },
+    { 'tpope/vim-surround', event = 'VeryLazy' },
+    { 'wellle/targets.vim', event = 'VeryLazy' },
     { 'airblade/vim-rooter' },
-    { 'michaeljsmith/vim-indent-object' },
-    { 'tpope/vim-sleuth' },
+    { 'michaeljsmith/vim-indent-object', event = 'VeryLazy' },
+    { 'tpope/vim-sleuth', event = 'VeryLazy' },
     {
         'numToStr/Comment.nvim',
         config = {},
