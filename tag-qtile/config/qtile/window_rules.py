2ccfc94fbf4793b18483a6caa6862c8b680fad6b
Łukasz Klimek <klinn91@gmail.com>
2024-01-07 20:28:49 +0100

    added rule for making scrpy windown floating

--- tag-qtile/config/qtile/window_rules.py
+++ tag-qtile/config/qtile/window_rules.py
@@ -4,5 +4,7 @@ def apply_rules(win: Window, wid: int, wm_class: str, proc_name: str):
     if wm_class.startswith('Gimp'):
         win.floating = True
         win.togroup('4')
+    elif wm_class == 'scrcpy':
+        win.floating = True
     elif wm_class == 'spotify' or proc_name == 'spotify':
         win.togroup('5')
