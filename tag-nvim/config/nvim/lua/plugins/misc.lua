447868d0d64f0059282b7c9d71eee72fefc7e7ab
Łukasz Klimek <lklimek@future-processing.com>
2024-04-10 07:27:52 +0200

    disabled copilot in text files

--- tag-nvim/config/nvim/lua/plugins/misc.lua
+++ tag-nvim/config/nvim/lua/plugins/misc.lua
@@ -112,6 +112,9 @@ return {
     {
         'github/copilot.vim',
         config = function()
+            vim.g.copilot_filetypes = {
+                text = false,
+            }
             vim.g.copilot_no_tab_map = true
             vim.keymap.set('i', '<M-Enter>', 'copilot#Accept("\\<CR>")', {
                 expr = true,
