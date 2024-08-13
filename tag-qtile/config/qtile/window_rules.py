e0bd163759e2e8fadd7a3309472eebd90cd00ddd
Łukasz Klimek <klinn91@gmail.com>
2024-08-13 18:40:16 +0200

    removed Gimp window rules - it hasn't been multi-window for a long time now

--- tag-qtile/config/qtile/window_rules.py
+++ tag-qtile/config/qtile/window_rules.py
@@ -1,10 +1,7 @@
 from libqtile.backend.x11.window import Window
 
 def apply_rules(win: Window, wid: int, wm_class: str, proc_name: str):
-    if wm_class.startswith('Gimp'):
-        win.floating = True
-        win.togroup('4')
-    elif wm_class == 'scrcpy':
+    if wm_class == 'scrcpy':
         win.floating = True
     elif wm_class == 'spotify' or proc_name == 'spotify':
         win.togroup('5')
