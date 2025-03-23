9cae4dff24cd4b8ecb1321505147ab87dfdf50bd
Łukasz Klimek <klinn91@gmail.com>
2025-03-23 16:13:40 +0100

    cleanup: removed some outdated pieces of config

--- tag-qtile/config/qtile/window_rules.py
+++
@@ -1,9 +0,0 @@
-from libqtile.backend.x11.window import Window
-
-def apply_rules(win: Window, wid: int, wm_class: str, proc_name: str):
-    if wm_class == 'scrcpy':
-        win.floating = True
-    elif wm_class == 'Mail':
-        win.togroup('3')
-    elif wm_class == 'spotify' or proc_name == 'spotify':
-        win.togroup('5')
