36006a5b4d1facc12ddee9d79c3225a2861c1bce
Łukasz Klimek <lklimek@future-processing.com>
2026-04-03 09:05:51 +0200

    feat(neovim): localconf now converts to lua, caches and refreshes based on mtime

--- tag-nvim/config/nvim/lua/user/localconf.lua
+++ tag-nvim/config/nvim/lua/user/localconf.lua
@@ -1,15 +1,39 @@
-local M = {
-    config = {},
-}
+local M = {}
+
+local env = vim.uv.os_environ()
+local config_home = env['XDG_CONFIG_HOME'] or (env['HOME'] .. '/.config/d')
+local cache_home = env['XDG_CACHE_HOME'] or (env['HOME'] .. '/.cache/d')
+local config_path = config_home .. '/config.toml'
+local cache_config_path = cache_home .. '/lua/config.lua'
+local cache_lua_dir = cache_home .. '/lua'
 
 function M.load()
-    local data = require('user.utils').exec('d config cat-json')
-    M.config = vim.json.decode(data)
-    return M.config
+    local config_stat = vim.uv.fs_stat(config_path)
+    if config_stat == nil then
+        vim.notify('Config file not found', vim.log.levels.WARN)
+        return
+    end
+
+    local luafied_config_stat = vim.uv.fs_stat(cache_config_path)
+    if luafied_config_stat == nil or config_stat.mtime.nsec > luafied_config_stat.mtime.nsec then
+        local cache_home_stat = vim.uv.fs_stat(cache_home)
+        if cache_home_stat == nil then
+            vim.uv.fs_mkdir(cache_home, tonumber('700', 8))
+        end
+
+        local cache_lua_dir_stat = vim.uv.fs_stat(cache_lua_dir)
+        if cache_lua_dir_stat == nil then
+            vim.uv.fs_mkdir(cache_lua_dir, tonumber('700', 8))
+        end
+
+        require('user.utils').exec('yq -ol ' .. config_path .. ' > ' .. cache_config_path)
+        package.loaded['config'] = nil -- invalidate the loaded module to force reloading
+    end
+    require('config')
 end
 
 function M.get(path, default)
-    local cur = M.config
+    local cur = require('config')
     for _, segment in ipairs(path) do
         cur = cur[segment]
         if cur == nil then
@@ -19,5 +43,6 @@ function M.get(path, default)
     return cur
 end
 
+vim.opt.rtp:prepend(cache_home)
 M.load()
 return M
