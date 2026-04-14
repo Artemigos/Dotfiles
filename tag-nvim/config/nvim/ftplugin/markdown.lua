0cc7632fcb6db23108e022bdc6f10bcc39fa49d2
Łukasz Klimek <klinn91@gmail.com>
2026-04-14 23:17:22 +0200

    fix(neovim): notify mdpls with textDocument/didOpen every time a buffer is entered
    this results in preview reliably tracking which file is on screen

--- tag-nvim/config/nvim/ftplugin/markdown.lua
+++ tag-nvim/config/nvim/ftplugin/markdown.lua
@@ -37,13 +37,26 @@ vim.api.nvim_create_user_command('MarkdownStopPreview', function()
     end
 end, {})
 
+local function make_did_open_params(buf)
+    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, true)
+    return {
+        textDocument = {
+            uri = vim.uri_from_bufnr(buf),
+            languageId = vim.bo[buf].filetype,
+            version = vim.lsp.util.buf_versions[buf],
+            text = table.concat(lines, '\n'),
+        },
+    }
+end
+
 vim.api.nvim_create_autocmd('BufEnter', {
-    buffer = 0,
-    callback = function()
+    buffer = vim.api.nvim_get_current_buf(),
+    callback = function(ev)
         local client = find_mdpls()
         if client then
-            vim.lsp.buf_attach_client(0, client.id)
+            vim.lsp.buf_attach_client(ev.buf, client.id)
+            -- NOTE: it's a little hacky (this is misuse of textDocument/didOpen), but the best way I found in mdpls's code
+            client:notify('textDocument/didOpen', make_did_open_params(ev.buf))
         end
     end,
-    group = vim.api.nvim_create_augroup('mdpls_attach_client', {}),
 })
